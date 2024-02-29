41ce5d95b5e60a0c8ead94e51c6d875cac367998
doylese <doylese@oregonstate.edu>
2024-02-28 22:07:11 -0800

    Backend Fixs

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
 const app = express()
-const PORT = 3000
+const PORT = 3001
 
 const UsersRoutes = require('./routes/UsersRoutes')
 const PostsRoutes = require('./routes/PostsRoutes')
 const GroupsRoutes = require('./routes/GroupsRoutes')
 const UserGroupsRoutes = require('./routes/UserGroupsRoutes')
 
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
