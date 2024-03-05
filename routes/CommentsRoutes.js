8dfb5c6d15b9808a2e3f07d5e7e7c99ccf17aefd
Howard Wang <howard122696@gmail.com>
2024-03-05 01:01:08 -0800

    comment controller and routes

--- routes/CommentsRoutes.js
+++ routes/CommentsRoutes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const CommentsController = require('../controller/CommentsController')
 
-
+router.get('/', CommentsController.getAllComments)
+router.post('/', CommentsController.addComment)
+router.delete('/:id', CommentsController.deleteCommentByID)
+router.put('/:id', CommentsController.editCommentByID)
 
 module.exports = router
