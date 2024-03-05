7b291f768094fe2cc8ec816776bf52b310ed55df
Howard Wang <howard122696@gmail.com>
2024-03-04 22:43:56 -0800

    Likes controller and routes

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ const UsersRoutes = require('./routes/UsersRoutes')
 const PostsRoutes = require('./routes/PostsRoutes')
 const GroupsRoutes = require('./routes/GroupsRoutes')
 const UserGroupsRoutes = require('./routes/UserGroupsRoutes')
+const CommentsRoutes = require('./routes/CommentsRoutes')
+const LikesRoutes = require('./routes/LikesRoutes')
 
 app.use(cors());
 app.use(express.json())
@@ -22,5 +24,7 @@ app.use('/users', UsersRoutes)
 app.use('/posts', PostsRoutes)
 app.use('/groups', GroupsRoutes)
 app.use('/user-groups', UserGroupsRoutes)
+app.use('/comments', CommentsRoutes)
+app.use('/likes', LikesRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) 
---
+++ routes/CommentsRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const CommentsController = require('../controller/CommentsController')
+
+
+
+module.exports = router
